010ecef0d29216cd05ff7c2a976c9e5776315c0e
preet006 <preetbhardwaj146@gmail.com>
2024-10-09 22:00:56 +0530

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,10 @@ app.use(express.urlencoded({
     limit: "16kb"
 }))
 
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.use(express.static("public"))
 app.use(cookieParser())
 
